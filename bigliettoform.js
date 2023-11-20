6b2919c24c39f93c1437933e505a4c16a788f306
Davide Prencipe <davidedev04@gmail.com>
2023-11-20 18:07:53 +0100

    aggiunti sconti

--- bigliettoform.js
+++ bigliettoform.js
@@ -3,6 +3,8 @@
 let nameUser = "valore iniziale vuoto";
 let kmUser = "valore iniziale vuoto";
 let ageUser = "valore iniziale vuoto";
+let prezzoBiglietto = "valore iniziale vuoto";
+let prezzoScontato = "valore iniziale vuoto";
 
 // riferimenti degli id
 const inputName = document.getElementById("user_name");
@@ -12,13 +14,27 @@ const buttonUser = document.getElementById("send_user");
 
 // funzione bottone
 buttonUser.addEventListener("click", function () {
-    // Assegna i valori degli input alle variabili
     nameUser = inputName.value;
     kmUser = inputKm.value;
     ageUser = inputAge.value;
 
-    // ENTRAAAAHHH???
     console.log("Nome Utente:", nameUser);
     console.log("Chilometri:", kmUser);
     console.log("Età:", ageUser);
+
+    // calcolo prezzo
+    const prezzoBiglietto = kmUser * 0.21;
+
+    // calcolo sconti
+    if (ageUser < 18) {
+        prezzoScontato = prezzoBiglietto - ((prezzoBiglietto * 20) / 100);
+
+    } else if (ageUser > 65) {
+        prezzoScontato = prezzoBiglietto - ((prezzoBiglietto * 40) / 100);
+    } else {
+        prezzoScontato = prezzoBiglietto * 1;
+    }
+
+    console.log(prezzoScontato);
 });
+
